feb67a4525c02f536db5c29ded96fedbc56241a5
Allyans3 <a.sylenok@gmail.com>
2019-06-13 20:29:19 +0300

    Update config.php

--- account/config.php
+++ account/config.php
@@ -1,19 +1,26 @@
 <?php
-    // $server = 'ricky.heliohost.org';
-    // $user = 'allyans3_auth';
-    // $pass = '54985754';
-    // $db = 'allyans3_auth';
-    $server = 'remotemysql.com';
-    $user = '0EhqmC71No';
-    $pass = 'o8F07QaCns';
-    $db = '0EhqmC71No';
+    $server = 'ricky.heliohost.org';
+    $user = 'allyans3_auth';
+    $pass = '54985754';
+    $db = 'allyans3_auth';
+    // $server = 'remotemysql.com';
+    // $user = '0EhqmC71No';
+    // $pass = 'o8F07QaCns';
+    // $db = '0EhqmC71No';
     // $server = 'localhost';
     // $user = 'root';
     // $pass = '';
     // $db = 'auth';
 
 
-    $connect = mysqli_connect($server, $user, $pass) or die("error");
+    $connect = mysqli_connect($server, $user, $pass);
 
-    $selectdb = mysqli_select_db($connect,$db) or die("error");
+    if (!$connect) {
+        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    $selectdb = mysqli_select_db($connect,$db) or die("error2");
 ?>
